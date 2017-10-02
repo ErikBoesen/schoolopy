2dbd743af7b7b76f942bf90b7cdd2b34c3ec13f4
ErikBoesen <me@erikboesen.com>
2017-10-01 22:42:16 -0400

    Basic auth framework

--- schoolopy/main.py
+++ schoolopy/main.py
@@ -1,2 +1,25 @@
+import requests
+import time
+import random
+
 class Schoology:
-    pass
+    key = ''
+    secret = ''
+
+    def _nonce(length=8):
+        return ''.join([str(random.randint(0, 9)) for i in range(length)])
+
+    def _oauth_header():
+        # TODO: Improve formatting here
+        auth = 'OAuth realm="Schoology API",'
+        auth += 'oauth_consumer_key="%s",' % key
+        auth += 'oauth_token="",'
+        auth += 'oauth_nonce="%s",' % nonce()
+        auth += 'oauth_timestamp="%d",' % time.time()
+        auth += 'oauth_signature_method="PLAINTEXT",'
+        auth += 'oauth_version="1.0",'
+        auth += 'oauth_signature="%s%%26"' % secret
+
+    def _request(path):
+        # FIXME: Returns encoded XML, obviously not final
+        return requests.get('https://api.schoology.com/v1/messages/inbox', headers={'Authorization': auth, 'Accept': 'text/xml;q=1.0,application/json;q=0.0;application/php;q=0.0', 'Host': 'api.schoology.com', 'Content-Type': 'text/xml'}).content
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-# Upload package to PyPi.
+# Uploading package to PyPi.
 
 from setuptools import setup
 
 setup(name='schoolopy',
-      version='0.1.1',
-      description='Finally, a Python wrapper for Schoology\'s API.',
+      version='0.1.0',
+      description='A Python wrapper for Schoology\'s API.',
       url='https://github.com/ErikBoesen/schoolopy',
       author='Erik Boesen',
       author_email='me@erikboesen.com',
