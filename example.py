ab3360455d814b58b72f5c92b60dba9ac484ffc1
ErikBoesen <me@erikboesen.com>
2017-10-04 18:04:40 -0400

    Simple homepage generator example

--- example.py
+++ example.py
@@ -1,30 +1,14 @@
 import schoolopy
 import yaml
 
+
 with open('example_config.yml', 'r') as f:
     cfg = yaml.load(f)
 
 sc = schoolopy.Schoology(cfg['key'], cfg['secret'])
 
-print('Most recent message:')
-print(sc.get_messages()[0])
-
-print('Your schools:')
-schools = sc.get_schools()
-print(schools)
-
-print('Your first school:')
-print(sc.get_school(schools[0].id))
-
-print('Your school\'s buildings:')
-print(sc.get_buildings(schools[0].id))
-
-print('The first user:')
-users = sc.get_users()
-print(users[0])
-
-print('The first user (fetched by ID):')
-print(sc.get_user(users[0].id))
-
-print('First group:')
-print(sc.get_groups())
+for update in sc.get_feed():
+    user = sc.get_user(update.uid)
+    print('By: ' + user.name_display)
+    print(update.body[:40].replace('\r\n', ' ').replace('\n', ' ') + '...')
+    print('%d likes\n' % update.likes)
