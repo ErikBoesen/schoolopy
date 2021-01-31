8773eb4a3a640fdf32b03bacc4f9d8ef2b05b1f0
AndrewLester <alester220@gmail.com>
2021-01-30 19:29:03 -0500

    Move NoDifferenceError, fix some methods, and add HTTP errors

--- schoolopy/models.py
+++ schoolopy/models.py
@@ -46,10 +46,4 @@ Session = _model('Session')
 MessageThread = _model('MessageThread')
 Message = _model('Message')
 Action = _model('Action')
-
-class NoDifferenceError(Exception):
-    """
-    This exception represents a case where schoology returns a String with the API
-    and makes it easier to tell what went wrong.
-    """
-    pass
+Role = _model('Role')
