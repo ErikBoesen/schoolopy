bb0235683f8cf3752292df3c6a36d573b73b7e50
ErikBoesen <me@erikboesen.com>
2022-03-27 16:18:40 -0400

    Bump to 0.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'r') as fh:
       long_description = fh.read()
 
 setup(name='schoolopy',
-      version='0.2.2',
+      version='0.2.3',
       description='A Python wrapper for Schoology\'s API.',
       long_description=long_description,
       url='https://github.com/ErikBoesen/schoolopy',
